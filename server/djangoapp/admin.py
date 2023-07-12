5346fdee344c7da631461890254ffc9186018165
Rafael Agas <rafael.raffy.agas@gmail.com>
2023-07-12 14:21:59 -0400

    Added make and car models and registered them"

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from .models import CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'body', 'make']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'country']
+    list_filter = ['country', 'year_established']
+    search_fields = ['name', 'country']
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,13 +3,22 @@ from django.utils.timezone import now
 
 
 # Create your models here.
-
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=50)
+    year_established = models.IntegerField(null=False)
+    country = models.CharField(null=False, max_length=30)
 
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+            "Country of Origin: " + self.country + ", " + \
+            "Year Established: " + str(self.year_established) + "," + \
+            "Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +28,19 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    body = models.CharField(null=False, max_length=30)
+    year = models.IntegerField()
+    dealer_Id = models.IntegerField()
+    make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+            "Body Type: " + self.body + ", " +\
+            "Year: " + str(self.year) + ", " + \
+            "Dealer: " + str(self.dealer_Id) + ", " + \
+            "Make: " + self.make.name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
