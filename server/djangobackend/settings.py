b436918d2b91cc898b75659efc47443ed405ad77
rafael Agas <rafael.raffy.agas@gmail.com>
2023-07-19 13:58:29 -0400

    added allowed hosts and csrf origins

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,9 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "localhost:8000", "https://rafaelraffya-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*"]
 
+CSRF_TRUSTED_ORIGINS = ["https://rafaelraffya-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/*"]
 
 # Application definition
 
